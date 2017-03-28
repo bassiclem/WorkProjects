266ee88638d426e72479d7a4dcad79033b441c69
bassi <bassiclem@hotmail.fr>
2017-03-28 15:03:55 +0200

    removed stuff

--- Project1/test.py
+++ Project1/test.py
@@ -8,24 +8,26 @@ M1 = Market()
 M2 = Market()
 M2.changer_enseigne("AH")
 L=["0x0","0x1","0x2","0x3","0x4","0x5","0x6","0x7","0x8","0x9","0xa","0xb","0xc","0xd","0xe"]
+
+
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         ref = Donnees("testsadds")
-
 
     def test_Donnees_sequencage(self):
         print("testing seq function")
         self.assertEqual(D.sequencage(3,3,3,2), [hex(int('100',2)),hex(int('110',2)),hex(int('01',2))])
         self.assertFalse(D.sequencage(3,3,3,2)==[hex(100),hex(110),hex(01)])
 
-
     def test_w2hex(self):
         print("testing w2hexa function")
         self.assertEqual(word2hexa("COOL"), [hex(ord("c")),hex(ord("o")),hex(ord("o")),hex(ord("l"))])
         self.assertFalse((word2hexa("COOL")==[hex(ord("C")),hex(ord("O")),hex(ord("O")),hex(ord("L"))]))
+
     def test_market_add(self):
         print("testing adding 2 market")
         self.assertEqual((M1+M2).Enseigne, "Monoprix et AH")
+
     def test_encoder_RSA(self):
         expected_return=['0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0', '0x0']
         print type(expected_return[0])
@@ -37,8 +39,7 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(TypeError):
             encoder_RSA([0x0, 0x0], 1, 1)
 
-        self.assertRaises(TypeError, encoder_RSA, ([0x0, 0x0], 1, 1)
-)
+        self.assertRaises(TypeError, encoder_RSA, ([0x0, 0x0], 1, 1))
 
 if __name__ == '__main__':
     unittest.main()
